fd788e31aa21bdf4f44860ea4db472b268edb65c
Abhay Deshpande <abhaydee>
2022-02-25 21:11:32 +0530

    updates to skills container icons

--- Components/Skills.jsx
+++ Components/Skills.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
-import styled from "styled-components"
-import { ReactLogo } from 'styled-icons/boxicons-logos';
-
+import React from "react";
+import styled from "styled-components";
+import { ReactLogo } from "styled-icons/boxicons-logos";
 
 function IconCard(props) {
   const { dropShadowColor, icon, title } = props;
@@ -24,44 +23,41 @@ function Skills() {
       <SkillsHeader>Skills</SkillsHeader>
       <IconCard
         dropShadowColor="#61dafb"
-        icon={
-          <ReactLogo width={80} height={80}/>
-        }
+        icon={<ReactLogo width={80} height={80} />}
         title="React"
       />
     </SkillsContainer>
-  )
+  );
 }
 const SkillsContainer = styled.div`
-   background-color: #242424;
-   padding: 2rem;
+  background-color: #242424;
+  padding: 2rem;
 `;
 
 const SkillsHeader = styled.h1`
-  color:#fff;
+  color: #fff;
   text-align: center;
-`
+`;
 const ShadowContainer = styled.div`
-  border-radius:9999px;
+  border-radius: 9999px;
   background-color: gray;
-  margin:0 auto;
-  width:112px;
-  height:112px;
-filter: ${(props) => `drop-shadow(0 0 0.75rem ${props.color}) !important`};
+  width:min-content;
+  margin-inline: auto;
+  background-color: rgba(243,244,246,1);
+  filter: ${(props) => `drop-shadow(0 0 0.75rem ${props.color}) !important`};
 `;
 
-const CustomReactLogo =styled(ReactLogo)`
+const CustomReactLogo = styled(ReactLogo)`
   height: 20px;
-  width:20px;
+  width: 20px;
   &:hover {
-      transform: scale(1.05);
-      transition: transform 1.05s ease-in-out;
-    }
-`
+    transform: scale(1.05);
+    transition: transform 1.05s ease-in-out;
+  }
+`;
 
 const IconContainer = styled.div`
-   min-width:min-content;
-   width:112px;
-   height:144px;
-`
+  min-width: min-content;
+  padding:1rem;
+`;
 export default Skills;
