2c27f48e31196dc60d23333530d176a99e972f67
abhaydee <abhaygirish1997@gmail.com>
2022-02-26 23:32:05 +0530

    removing unwanted functions

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -3,16 +3,13 @@ import React from "react";
 import { Header3, HeaderContainer } from "../utils/CustomComponents";
 
 function Header() {
-  const navigateToResume = () => {
-    window.open("https://nonchalant-trouble-13d.notion.site/Abhay-G-Deshpande-c47fee9ba740423a9bcc62274b1cdd56","_blank")
-  };
   return (
     <>
       <HeaderContainer>
         <Header3 href="#">ABHAY DESHPANDE</Header3>
         <Header3 href="#projects">PROJECTS</Header3>
         <Header3 href="#contact">CONTACT</Header3>
-        <Header3 onClick={navigateToResume}>RESUME</Header3>
+        <Header3 href={"https://www.notion.so/Abhay-G-Deshpande-c47fee9ba740423a9bcc62274b1cdd56"} target="_blank" rel="noopener noreferrer">RESUME</Header3>
       </HeaderContainer>
     </>
   );
