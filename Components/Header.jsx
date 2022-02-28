6c0a79db97bb14b45450bd200251838ce61dbaba
abhaydee <abhaygirish1997@gmail.com>
2022-03-01 00:23:07 +0530

    changes to resume link

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
         <Header3 href="#">ABHAY DESHPANDE</Header3>
         <Header3 href="#projects">PROJECTS</Header3>
         <Header3 href="#contact">CONTACT</Header3>
-        <Header3 href={"https://www.notion.so/Abhay-G-Deshpande-c47fee9ba740423a9bcc62274b1cdd56"} target="_blank" rel="noopener noreferrer">RESUME</Header3>
+        <Header3 href={"/Abhay-Deshpande_CV.html"} target="_blank" rel="noopener noreferrer">RESUME</Header3>
       </HeaderContainer>
     </>
   );
